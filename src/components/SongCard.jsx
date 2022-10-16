727e0749e0945549c17f864564511cc36ea3a75f
ps-lolfan <pbsinnur@gmail.com>
2022-10-16 12:49:45 +0530

    music player functionality done

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,9 +5,16 @@ import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
-  const handlePauseClick = () => {};
+  const dispatch = useDispatch();
 
-  const handlePlayClick = () => {};
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
@@ -15,7 +22,7 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
             activeSong?.title === song.title
-              ? "felx bg-black bg-opacity-70"
+              ? "flex bg-black bg-opacity-70"
               : "hidden"
           }`}
         >
